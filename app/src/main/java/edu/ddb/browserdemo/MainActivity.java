43e2799f60c7f7a92a8ab3b553053e41ed1fa4c6
Dean Bartel <300089145@fvtc.edu>
2024-02-01 12:09:24 -0600

    added browser stuff. Load a website and create custom html

--- app/src/main/java/edu/ddb/browserdemo/MainActivity.java
+++ app/src/main/java/edu/ddb/browserdemo/MainActivity.java
@@ -1,9 +1,15 @@
 package edu.ddb.browserdemo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,8 +19,10 @@ import androidx.navigation.ui.NavigationUI;
 import edu.ddb.browserdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+ public static final String TAG = "MainActivity";
 
     private ActivityMainBinding binding;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,42 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        navView.setOnItemSelectedListener(onItemSelectedListener);
+
+        webView = findViewById(R.id.webView);
+
     }
+
+    private NavigationBarView.OnItemSelectedListener onItemSelectedListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Log.d(TAG, "onNavigationItemSelected: ");
+
+            int id = item.getItemId();
+
+            if(id == R.id.navigation_dashboard)
+            {
+                // Show the contents of a url.
+                webView.setWebViewClient(new WebViewClient());
+                webView.loadUrl("https://www.fvtc.edu/");
+            } else if (id == R.id.navigation_notifications)
+            {
+                // construct html
+                String html = "<html><body style='background-color:cornflowerblue'>";
+                html += "<table border='1'>";
+                html += "<tr><td>Bart</td></tr>";
+                html += "<tr><td>Marge</td></tr>";
+                html += "<tr><td>Homer</td></tr>";
+                html += "<tr><td>Lisa</td></tr>";
+                html += "<tr><td>Mo</td></tr>";
+                html += "</table></body></html>";
+
+                webView.loadData(html, "text/html", "UTF-8");
+            }
+
+            return true;
+        }
+    };
 
 }
